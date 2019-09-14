94b3b05544bf935b48b30718204f9b61acc2cc39
Denis Glotov <denis@glotov.org>
2019-09-14 15:09:56 +0300

    initial ethers fix

--- tests/test.sh
+++ tests/test.sh
@@ -4,6 +4,11 @@ cd $(dirname $0)
 dymka version
 dymka balance
 
+if [ ! -e WEB3_FROM ]; then
+    echo "WEB3_FROM must be set for this script to run"
+    exit 1
+fi
+
 # Deploy demo contract.
 ADDR=$(dymka -c demo deploy | jq -r ".receipt.contractAddress")
 export WEB3_CONTRACT_DEMO="$ADDR"
