38eb4842071ac63f9b1b9077d014f5cc8e53999e
Denis Glotov <denis@glotov.org>
2019-09-14 15:28:05 +0300

    More tests

--- tests/test.sh
+++ tests/test.sh
@@ -4,14 +4,22 @@ cd $(dirname $0)
 dymka version
 dymka balance
 
-if [ ! -v WEB3_FROM ]; then
-    echo "WEB3_FROM must be set for this script to run"
-    exit 1
-fi
+# From account must be set by travis beforehand.
+[ -v WEB3_FROM ] || ( echo "WEB3_FROM must be set for this script to run" && exit 1 )
 
 # Deploy demo contract.
 ADDR=$(dymka -c demo deploy | jq -r ".receipt.contractAddress")
 export WEB3_CONTRACT_DEMO="$ADDR"
 
-# Call value()
-dymka -c demo call value
+# Call value() function.
+RES=$(dymka -c demo call value | jq -r ".result")
+[ "$RES" == "42" ] || ( echo "Wrong initial value" && exit 1 )
+
+# Invoke act() function.
+dymka -c demo send act
+
+# Call value() function.
+RES=$(dymka -c demo call value | jq -r ".result")
+[ "$RES" == "43" ] || ( echo "Wrong initial value" && exit 1 )
+
+echo "Success."
