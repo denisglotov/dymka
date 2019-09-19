c2ac87bbc46422c6e6cb64b4bb3f3cd76794455b
Denis Glotov <denis@glotov.org>
2019-09-19 16:27:49 +0300

    [1.0.3] Receipt logs processing, readme updated

--- tests/test.sh
+++ tests/test.sh
@@ -12,32 +12,40 @@ dymka balance
 # From account must be set by travis beforehand.
 [ -v WEB3_FROM ] || die "WEB3_FROM must be set for this script to run"
 
+echo
 echo "Deploy Demo contract."
 ADDR=$(dymka -c demo deploy | jq -r ".receipt.contractAddress")
 export WEB3_CONTRACT_DEMO="$ADDR"
 
+echo
 echo "Call its value() function."
 RES=$(dymka -c demo call value | jq -r ".result")
 [ "$RES" == "42" ] || die "Wrong initial value $RES"
 
+echo
 echo "Invoke its act() function."
 dymka -c demo send act
 
+echo
 echo "Call value() function again. Must be 43."
 RES=$(dymka -c demo call value | jq -r ".result")
 [ "$RES" == "43" ] || die "Wrong value $RES"
 
+echo
 echo "Call compare(45) function."
 RES=$(dymka -c demo call compare 45 | jq -r ".result")
 [ $(echo "$RES" | tr -d '[:space:]') == "[false,true]" ] || die "Wrong $RES"
 
+echo
 echo "Invoke its set(43, 100) function."
 dymka -c demo send set 43 100
 
+echo
 echo "Call value() function. Must be 100."
 RES=$(dymka -c demo call value | jq -r ".result")
 [ "$RES" == "100" ] || die "Wrong value $RES"
 
+echo
 echo "Invoke its teardown() function."
 dymka -c demo send teardown "'0x0000000000000000000000000000000000000000'"
 
